fc11bf40e6d8008ccac06dca52b01d42a70c69de
claire Roughan <claire_roughan@yahoo.co.uk>
2017-02-13 11:29:07 +0000

    Detail viewController

--- NewsBulletin/NewsReaderViewController.swift
+++ NewsBulletin/NewsReaderViewController.swift
@@ -8,28 +8,51 @@
 
 import UIKit
 
-class NewsReaderViewController: UIViewController {
+class NewsReaderViewController: UIViewController, UIWebViewDelegate {
 
+    @IBOutlet weak var webView: UIWebView!
+    
+    var urlString:String?
+    
+    var activityIndicatorView: ActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        showActivityIndicator()
 
-        // Do any additional setup after loading the view.
+        let url = NSURL (string: urlString!)
+        
+        print("\n\n \(urlString)\n\n")
+        
+        let requestObj = URLRequest(url: url! as URL)
+        webView.loadRequest(requestObj)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // MARK: - UIWebViewDelegate
+    func webViewDidFinishLoad(_ webView : UIWebView) {
+        self.activityIndicatorView.stopAnimating()
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    func showActivityIndicator() {
+        
+//        self.activityIndicatorView = ActivityIndicatorView(title: "Loading news article ...",
+//                                                           center: self.view.center,
+//                                                           bgColour: UIColor(red: 126.0/255.0, green: 125.0/255.0, blue: 170.0/255.0, alpha: 0.5),
+//                                                           textColour:UIColor.purple)
+        
+        
+         self.activityIndicatorView = ActivityIndicatorView(title: "Loading news article ...",
+                                                            center: self.view.center,
+                                                            width: 250,
+                                                            bgColour:  UIColor(red: 126.0/255.0, green: 125.0/255.0, blue: 170.0/255.0, alpha: 0.5),
+                                                            textColour: UIColor.purple)
+        
+        self.view.addSubview(self.activityIndicatorView.getViewActivityIndicator())
+        self.activityIndicatorView.startAnimating()
     }
-    */
+
 
 }
